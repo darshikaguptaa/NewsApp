6e16c4c497bfa144d55eda5a95ce13f3e4222de3
darshikaguptaa <dng2804@gmail.com>
2022-08-05 13:52:15 +0530

    Updates

---
+++ precache-manifest.d1541967006fa834ed1432e4809c1770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3562fb21c8a1b8dc4d4b",
+    "url": "/NewsApp/static/css/main.fc4b4d1c.chunk.css"
+  },
+  {
+    "revision": "3562fb21c8a1b8dc4d4b",
+    "url": "/NewsApp/static/js/main.3562fb21.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/NewsApp/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "42e42d3d7108ffed23b5",
+    "url": "/NewsApp/static/js/2.42e42d3d.chunk.js"
+  },
+  {
+    "revision": "048e3ed943688bbac9d2",
+    "url": "/NewsApp/static/js/runtime~main.048e3ed9.js"
+  },
+  {
+    "revision": "22043e8c7084d85807c5d97cfe604d27",
+    "url": "/NewsApp/static/media/loading.22043e8c.gif"
+  },
+  {
+    "revision": "0c363cade8e5429daa1c8f0e1d24e370",
+    "url": "/NewsApp/index.html"
+  }
+];
